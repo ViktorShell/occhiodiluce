573ab9e45fa536f99668e4655e9b73079b2f7e61
viktorshell <viktorshell>
2024-06-18 20:38:46 +0200

    Firebase auth

--- src/lib/Register.svelte
+++ src/lib/Register.svelte
@@ -1,12 +1,16 @@
 <script>
+  import { createUserWithEmailAndPassword } from 'firebase/auth';
   import mementomori from '../assets/mementomori.jpg';
 
   export let auth;
   let mail = "";
-  let password = "";
+  let passwd = "password";
 
   function registerHandler(){
-    
+    createUserWithEmailAndPassword(auth, mail, passwd)
+      .then((userCredential) => {
+
+      })
   }
 </script>
 
@@ -27,7 +31,7 @@
       <p class="text-lg pl-1 pb-2">Password</p>
       <label class="input input-bordered flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
-        <input type="password" class="grow" value="password" bind:value = {password} />
+        <input type="password" class="grow" bind:value = {passwd} />
       </label>
       <br>
       <div style = "witdh:100%; display: flex; justify-content: space-between">
