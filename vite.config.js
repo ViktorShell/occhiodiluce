47255d195d09ddd0c134965a896f8ccb9b1a7530
viktorshell <viktorshell>
2024-06-25 16:36:58 +0200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      manifest: {
+        name: "Occhio di Luce",
+        icons: [
+          {
+            src: "src/assets/eye_192.jpg",
+            size: "192x192",
+            type: "image/jpg",
+            purpose: "any maskable"
+          },
+          {
+            src: "src/assets/eye_512.jpg",
+            size: "512x512",
+            type: "image/jpg",
+            purpose: "any maskable"
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: {
+          urlPattern: ({ url }) => {
+            return true;
+          },
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "v1",
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      }
+    })
+    ],
 })
